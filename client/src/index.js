bed33cbfefefc125c1ad854a7d4009da35a6ae94
David Caldwell <doubledc92@gmail.com>
2022-12-09 10:28:14 -0500

    still setting up and getting rid of unused code

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import { BrowerRouter } from 'react-router-dom'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <BrowerRouter>
+      <App />
+    </BrowerRouter>
+  </React.StrictMode>
+)
