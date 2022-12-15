e222454e055ebdbc994eef6da21b875471d72dfc
David Caldwell <doubledc92@gmail.com>
2022-12-15 08:44:29 -0500

    forgot there was nothing on the about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='about'>
+      <p>The Dive Buddy is an app to keep dive logs and create loadouts to use as gear checklists before a dive trip.</p>
+        
+      </div>
   )
 }
 
